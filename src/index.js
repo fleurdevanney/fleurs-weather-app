70615762c98a495efc622fb151b51eb4abd74ca5
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-03-18 10:29:11 +0000

    updates to axios & css styling

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "16bfc20460o66fe0f260tfe95532783a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function searchAction(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
