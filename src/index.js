880f853b234dabde66b29fef98e13a2d879e9ff3
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-03-19 12:22:42 +0000

    updated the JS file to display current date/time for search value

--- src/index.js
+++ src/index.js
@@ -6,15 +6,41 @@ function updateWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#speed");
   let speed = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${Math.round(speed)}mph`;
+  timeElement.innerHTML = formatDate(date);
 
-  console.log(response.data.wind.speed);
+  console.log(response.data.time);
 }
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "16bfc20460o66fe0f260tfe95532783a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
