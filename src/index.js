ad89a452e1be01497472846b20f648811bf33bfa
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-03-19 10:20:21 +0000

    updated the JS to replace the city/temperature with the search value

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 function searchCity(city) {
   let apiKey = "16bfc20460o66fe0f260tfe95532783a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function searchAction(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
