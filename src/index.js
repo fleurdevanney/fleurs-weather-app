36bdda3020029c25821b9a6bbb17fb371769bf79
fleurdevanney <fleurdevanney@hotmail.co.uk>
2025-03-19 11:15:16 +0000

    updated the JS to display weather desc/wind speed/humidity of search value

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let speedElement = document.querySelector("#speed");
+  let speed = response.data.wind.speed;
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  speedElement.innerHTML = `${Math.round(speed)}mph`;
+
+  console.log(response.data.wind.speed);
 }
 function searchCity(city) {
   let apiKey = "16bfc20460o66fe0f260tfe95532783a";
